50686e6cdd0c67eb687cf21186027de22dd50823
grozzzny <1234578>
2018-02-26 11:51:55 +0200

    add favicon and mixins

--- views/layouts/base.php
+++ views/layouts/base.php
@@ -9,6 +9,8 @@ $asset = \app\assets\AppAsset::register($this);
         <meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?= Html::csrfMetaTags() ?>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
