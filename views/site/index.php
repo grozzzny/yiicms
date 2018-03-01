1aa69ec0c1d33c39955ab11475fc2d797cbb8978
grozzzny <1234578>
2018-03-01 09:14:34 +0200

    png

--- views/site/index.php
+++ views/site/index.php
@@ -77,7 +77,7 @@ $this->title = $page->seo('title', $page->model->title);
             <div class="row">
                 <div class="col-sm-6 col-md-4">
                     <div class="thumbnail animated fadeIn wow" data-wow-duration="1s" data-wow-delay="0s"  data-wow-offset="200">
-                        <img src="/images/nophoto.jpg" alt="">
+                        <img src="/images/nophoto.png" alt="">
                         <div class="caption">
                             <h3>Thumbnail label</h3>
                             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -87,7 +87,7 @@ $this->title = $page->seo('title', $page->model->title);
                 </div>
                 <div class="col-sm-6 col-md-4">
                     <div  class="thumbnail animated fadeIn wow" data-wow-duration="1s" data-wow-delay="0.5s"  data-wow-offset="200">
-                        <img src="/images/nophoto.jpg" alt="">
+                        <img src="/images/nophoto.png" alt="">
                         <div class="caption">
                             <h3>Thumbnail label</h3>
                             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -97,7 +97,7 @@ $this->title = $page->seo('title', $page->model->title);
                 </div>
                 <div class="col-sm-6 col-md-4">
                     <div class="thumbnail animated fadeIn wow" data-wow-duration="1s" data-wow-delay="1s"  data-wow-offset="200">
-                        <img src="/images/nophoto.jpg" alt="">
+                        <img src="/images/nophoto.png" alt="">
                         <div class="caption">
                             <h3>Thumbnail label</h3>
                             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
