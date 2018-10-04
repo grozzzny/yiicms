650ab0d4aa716c5528ff2236c4bb11b19d425ddd
grozzzny <1234578>
2018-10-04 13:31:34 +0200

    smtp

--- config/smtp-example.php
+++ config/smtp-example.php
@@ -7,4 +7,11 @@ return [
     'password' => '',
     'port' => '587', //587 || 465
     'encryption' => 'TLS', //tls || ssl
+//    'streamOptions' => [
+//        'ssl' => [
+//            'allow_self_signed' => true,
+//            'verify_peer' => false,
+//            'verify_peer_name' => false,
+//        ],
+//    ],
 ];
